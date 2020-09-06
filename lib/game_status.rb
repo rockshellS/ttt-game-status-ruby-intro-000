74919a1ee0fdc00d21631d4256db51a1698cc8aa
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-06 19:32:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,4 +16,10 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if 
+  WIN_COMBINATIONS.each do |win_combo|
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+    
+    
+    
