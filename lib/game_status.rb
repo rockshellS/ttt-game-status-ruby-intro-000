d1b888cf6232c9c71a752ce5f539a7a05cef325a
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-06 19:35:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,5 +21,13 @@ def won?(board)
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
     
+    position_1 == board[win_index_1]
+    position_2 == board[win_index_2]
+    position_3 == board[win_index_3]
     
-    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    else 
+      false
+    end
+end
